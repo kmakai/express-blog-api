1458c3a9e0d9cc0fec5d06ea97cfec384d3720a4
kmakai <makaikwag@gmail.com>
2023-01-16 21:14:28 -0500

    installed cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 
@@ -13,6 +14,7 @@ const app = express();
 // Connect to database
 connectDB();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
